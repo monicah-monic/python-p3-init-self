3be3b51b62e10128710ecfcec08406b8a38a46d0
monicah-monic <monicahwanjiru205@gmail.com>
2025-05-16 07:02:44 +0300

    self __init__

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed= "Mutt"):
+        self.name = name
+        self.breed = breed
+        print(f"{name}")
+        print(f"{breed}")
+
+dog = Dog("Fido", "Dalmatian")
+dog.name
+dog.breed      
+        
